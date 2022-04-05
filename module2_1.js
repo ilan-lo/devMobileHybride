765f0e1c301d1937d79569995d26ef5ef2ed76fe
ilan <e@e.e>
2022-04-05 10:45:57 +0200

    'upload'

--- module2_1.js
+++ module2_1.js
@@ -89,7 +89,7 @@ class Savoir{
         console.log(objetobjetLu);
     }
 }
-
+let j = 0;
 let allSavoir =[];
 //on rajoute celle qu'on connais deja
 while (localStorage.getItem("objet"+i) != null || i < 100) {
@@ -102,9 +102,38 @@ while (localStorage.getItem("objet"+i) != null || i < 100) {
         allSavoir.push(nouv);
         nouv.ajouterDansleLocal();
         console.log(allSavoir);
+        j = i;
     }else i++;
 }
+i=j;
 
+
+//trie
+function compareValues(key, order = 'asc') {
+    return function innerSort(a, b) {
+        if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+            // property doesn't exist on either object
+            return 0;
+        }
+
+        const varA = (typeof a[key] === 'string')
+            ? a[key].toUpperCase() : a[key];
+        const varB = (typeof b[key] === 'string')
+            ? b[key].toUpperCase() : b[key];
+
+        let comparison = 0;
+        if (varA > varB) {
+            comparison = 1;
+        } else if (varA < varB) {
+            comparison = -1;
+        }
+        return (
+            (order === 'desc') ? (comparison * -1) : comparison
+        );
+    };
+}
+allSavoir.sort(compareValues('date'));
+console.log(allSavoir);
 
 while (localStorage.getItem("objet"+i)!=null) {
     i++;
