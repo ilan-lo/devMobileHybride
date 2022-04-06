2933d3cd4f8b8686ff231373076e3c19a1e64c37
ilan <e@e.e>
2022-04-06 11:17:19 +0200

    'add'

--- ModulesTPcorrifé.js
+++ ModulesTPcorrifé.js
@@ -1,136 +1,23 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 exports.__esModule = true;
-exports.VenteAuxEncheres = void 0;
-var VenteAuxEncheres;
-(function (VenteAuxEncheres) {
-    // ##########
-    // Classes
-    // ##########
-    var Acheteur = /** @class */ (function () {
-        function Acheteur(nom, prenom) {
-            this.nom = nom;
-            this.prenom = prenom;
-        }
-        return Acheteur;
-    }());
-    VenteAuxEncheres.Acheteur = Acheteur;
-    // Classe Auteur qui implémente Affichable
-    // Doit donc contenir une méthode afficher(): string
-    var Auteur = /** @class */ (function () {
-        // Constructeur qui définit les attributs
-        function Auteur(nom, prenom) {
-            this.nom = nom;
-            this.prenom = prenom;
-        }
-        // On implémente Affichable, on doit avoir cette méthode
-        Auteur.prototype.afficher = function () {
-            // Renvoie une string
-            return this.nom + " " + this.prenom;
-        };
-        return Auteur;
-    }());
-    VenteAuxEncheres.Auteur = Auteur;
-    var Oeuvre = /** @class */ (function () {
-        // Attributs sont déclarés et initialisés dès le constructeur
-        function Oeuvre(annee, titre, auteurs) {
-            this.annee = annee;
-            this.titre = titre;
-            this.auteurs = auteurs;
-        }
-        // On pourra instancier une oeuvre ainsi:
-        /*
-            let oeuvreGeniale: Oeuvre = new Oeuvre(
-                2022,
-                "CESI Brest",
-                [ new Auteur("Lansonneur", "David"), new Auteur("Perchirin", "Loan"), new Auteur("Queron", "Mathéo" )]
-            );
-        */
-        // Implémente Affichable
-        Oeuvre.prototype.afficher = function () {
-            // Variable qui contiendra la liste des auteurs sous forme de string
-            var stringAuteurs;
-            // Parcours du tableau des auteurs
-            if (this.auteurs != undefined) {
-                // Initialise la string a vide
-                stringAuteurs = "";
-                // Expression lambda de parcours du tableau en une seule ligne
-                this.auteurs.forEach(function (value) { return stringAuteurs += (stringAuteurs == "" ? "" : ", ") + value.afficher(); });
-                // Sur plusieurs lignes, ça ferait:
-                /*
-                    this.auteurs.forEach(
-                        function (auteurEnCours, index, tableauComplet ){
-                            // Concaténation de stringAuteurs
-                            if ( stringAuteurs == "" )
-                            {
-                                stringAuteurs += "";
-                            }else{
-                                stringAuteurs +=  ", ";
-                            }
-                            // Dans tous les cas, on ajoute le contenu de l'auteur
-                            stringAuteurs +=  auteurEnCours.afficher();
-                        }
-                    );
-                */
-            }
-            // Renvoi final
-            return "[annee : " + this.annee +
-                ", titre : " + this.titre +
-                ", auteurs : [" + stringAuteurs + "]" +
-                (this.meilleureEnchere != undefined ? (", meilleure enchère : " + this.meilleureEnchere) : "") +
-                (this.acheteur != undefined ? (", acheteur : " + this.acheteur.nom + " " + this.acheteur.prenom) : "") +
-                "]";
-        };
-        // Methode encherir exigée par Vendable
-        Oeuvre.prototype.encherir = function (nouvelleEnchere, acheteur) {
-            this.meilleureEnchere = nouvelleEnchere;
-            this.acheteur = acheteur;
-        };
-        return Oeuvre;
-    }());
-    VenteAuxEncheres.Oeuvre = Oeuvre;
-    // Sculpture hérite de Oeuvre
-    var Sculpture = /** @class */ (function (_super) {
-        __extends(Sculpture, _super);
-        // Même constructeur que le parent, mais en plus, on a la matière
-        function Sculpture(annee, titre, auteurs, matiere) {
-            var _this = 
-            // Appel au constructeur parent
-            _super.call(this, annee, titre, auteurs) || this;
-            _this.matiere = matiere;
-            return _this;
-        }
-        return Sculpture;
-    }(Oeuvre));
-    VenteAuxEncheres.Sculpture = Sculpture;
-    var Livre = /** @class */ (function (_super) {
-        __extends(Livre, _super);
-        function Livre(annee, titre, auteurs) {
-            return _super.call(this, annee, titre, auteurs) || this;
-        }
-        return Livre;
-    }(Oeuvre));
-    VenteAuxEncheres.Livre = Livre;
-    var Peinture = /** @class */ (function (_super) {
-        __extends(Peinture, _super);
-        function Peinture(annee, titre, auteurs) {
-            return _super.call(this, annee, titre, auteurs) || this;
-        }
-        return Peinture;
-    }(Oeuvre));
-    VenteAuxEncheres.Peinture = Peinture;
-})(VenteAuxEncheres = exports.VenteAuxEncheres || (exports.VenteAuxEncheres = {}));
+var VenteAuxEncheres_1 = require("./VenteAuxEncheres");
+// Création des oeuvres
+var oeuvres = [];
+oeuvres.push(new VenteAuxEncheres_1.VenteAuxEncheres.Sculpture(1925, "Le penseur de rodin", [
+    new VenteAuxEncheres_1.VenteAuxEncheres.Auteur("Rodin", "Auguste")
+], "bronze"));
+oeuvres.push(new VenteAuxEncheres_1.VenteAuxEncheres.Peinture(1995, "David", [
+    new VenteAuxEncheres_1.VenteAuxEncheres.Auteur("papa", "de david"),
+    new VenteAuxEncheres_1.VenteAuxEncheres.Auteur("maman", "de david")
+]));
+oeuvres.push(new VenteAuxEncheres_1.VenteAuxEncheres.Livre(1998, "Coupe du monde", [
+    new VenteAuxEncheres_1.VenteAuxEncheres.Auteur("Zidane", "Zinedine"),
+    new VenteAuxEncheres_1.VenteAuxEncheres.Auteur("Barthez", "Fabien")
+]));
+//Encheres
+oeuvres.forEach(function (oeuvre) {
+    oeuvre.encherir(Math.floor(Math.random() * 10000), new VenteAuxEncheres_1.VenteAuxEncheres.Acheteur("Bara", "Morgane"));
+});
+console.log(oeuvres);
+//Parcours et affichage des oeuvres
+oeuvres.forEach(function (oeuvres) { return console.log(oeuvres.afficher()); });
--- ModulesTPcorrifé.ts
+++ ModulesTPcorrifé.ts
@@ -1,131 +1,69 @@
-export module VenteAuxEncheres
-{
-    // ############
-    // Interfaces
-    // ############
-    export interface Affichable
-    {
-        // On oblige les classes (ou objets) de contenir (de définir) une méthode afficher qui doit renvoyer du string
-        afficher() : string;
+import { VenteAuxEncheres} from "./VenteAuxEncheres";
+// Création des oeuvres
+
+let oeuvres : VenteAuxEncheres.Oeuvre[] = [];
+
+oeuvres.push(
+    new VenteAuxEncheres.Sculpture(
+        1925,
+        "Le penseur de rodin",
+        [
+            new VenteAuxEncheres.Auteur(
+                "Rodin",
+                "Auguste"
+            )
+        ],
+        "bronze"
+    )
+);
+
+oeuvres.push(
+    new VenteAuxEncheres.Peinture(
+        1995,
+        "David",
+        [
+            new VenteAuxEncheres.Auteur(
+                "papa",
+                "de david"
+            ),
+            new VenteAuxEncheres.Auteur(
+                "maman",
+                "de david"
+            )
+        ],
+    )
+);
+oeuvres.push(
+    new VenteAuxEncheres.Livre(
+        1998,
+        "Coupe du monde",
+        [
+            new VenteAuxEncheres.Auteur(
+                "Zidane",
+                "Zinedine"
+            ),
+            new VenteAuxEncheres.Auteur(
+                "Barthez",
+                "Fabien"
+            )
+        ],
+    )
+);
+
+
+//Encheres
+oeuvres.forEach(
+    (oeuvre) => {
+        oeuvre.encherir(
+            Math.floor(Math.random() * 10000),
+            new VenteAuxEncheres.Acheteur(
+                "Bara", "Morgane"
+            )
+        )
     }
-    export interface Vendable
-    {
-        // Attribut attendu
-        meilleureEnchere : number;
-        // Attribut de liaison (0..1)
-        acheteur ?: Acheteur;
-        // Méthode encherir
-        encherir ( nouvelleEnchere : number, acheteur : Acheteur );
-    }
-    // ##########
-    // Classes
-    // ##########
-    export class Acheteur
-    {
-        constructor( public nom : string, public prenom : string ){}
-    }
-    // Classe Auteur qui implémente Affichable
-    // Doit donc contenir une méthode afficher(): string
-    export class Auteur implements Affichable
-    {
-        // Constructeur qui définit les attributs
-        constructor(public nom : string, public prenom : string){}
-        // On implémente Affichable, on doit avoir cette méthode
-        afficher(): string
-        {
-            // Renvoie une string
-            return this.nom + " " + this.prenom;
-        }
-    }
-    export class Oeuvre implements Affichable, Vendable
-    {
-        // Attributs exigés par Vendable
-        meilleureEnchere : number;
-        acheteur ?: Acheteur;
-        // Attributs sont déclarés et initialisés dès le constructeur
-        constructor(
-            public annee : number,
-            public titre : string,
-            public auteurs : Auteur[]
-        ){}
-        // On pourra instancier une oeuvre ainsi:
-        /*
-            let oeuvreGeniale: Oeuvre = new Oeuvre(
-                2022,
-                "CESI Brest",
-                [ new Auteur("Lansonneur", "David"), new Auteur("Perchirin", "Loan"), new Auteur("Queron", "Mathéo" )]
-            );
-        */
-        // Implémente Affichable
-        afficher() : string
-        {
-            // Variable qui contiendra la liste des auteurs sous forme de string
-            let stringAuteurs : string;
-            // Parcours du tableau des auteurs
-            if( this.auteurs!=undefined )
-            {
-                // Initialise la string a vide
-                stringAuteurs = "";
-                // Expression lambda de parcours du tableau en une seule ligne
-                this.auteurs.forEach((value)=>stringAuteurs+=(stringAuteurs==""?"":", ") + value.afficher());
-                // Sur plusieurs lignes, ça ferait:
-                /*
-                    this.auteurs.forEach(
-                        function (auteurEnCours, index, tableauComplet ){
-                            // Concaténation de stringAuteurs
-                            if ( stringAuteurs == "" )
-                            {
-                                stringAuteurs += "";
-                            }else{
-                                stringAuteurs +=  ", ";
-                            }
-                            // Dans tous les cas, on ajoute le contenu de l'auteur
-                            stringAuteurs +=  auteurEnCours.afficher();
-                        }
-                    );
-                */
-            }
-            // Renvoi final
-            return "[annee : " + this.annee +
-                ", titre : " + this.titre +
-                ", auteurs : [" + stringAuteurs + "]" +
-                ( this.meilleureEnchere!=undefined?(", meilleure enchère : " + this.meilleureEnchere) : "") +
-                ( this.acheteur!= undefined? (", acheteur : " + this.acheteur.nom+" " + this.acheteur.prenom):"") +
-                "]";
-        }
-        // Methode encherir exigée par Vendable
-        encherir( nouvelleEnchere : number, acheteur : Acheteur ) : void
-        {
-            this.meilleureEnchere = nouvelleEnchere;
-            this.acheteur = acheteur;
-        }
-    }
-    // Sculpture hérite de Oeuvre
-    export class Sculpture extends Oeuvre
-    {
-        // Même constructeur que le parent, mais en plus, on a la matière
-        constructor(annee : number, titre : string, auteurs : Auteur[], public matiere : string)
-        {
-            // Appel au constructeur parent
-            super(annee, titre, auteurs);
-        }
-        // Pour un instance de sculpture, on aura:
-        /*
-            let sculpture = new Sculpture(2021, "CESI", [ new Auteur("Fer", "Brivael") ], "fer" );
-        */
-    }
-    export class Livre extends Oeuvre
-    {
-        constructor(annee : number, titre : string, auteurs : Auteur[])
-        {
-            super(annee, titre, auteurs);
-        }
-    }
-    export class Peinture extends Oeuvre
-    {
-        constructor(annee : number, titre : string, auteurs : Auteur[])
-        {
-            super(annee, titre, auteurs);
-        }
-    }
-}
+)
+console.log(oeuvres);
+//Parcours et affichage des oeuvres
+oeuvres.forEach(
+    (oeuvres)=>console.log(oeuvres.afficher())
+);
